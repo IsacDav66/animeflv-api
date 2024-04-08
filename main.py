18f3a0a887c0fcfb8272d36837ba6a036d6fb33a
IsacDav66 <96359209+IsacDav66@users.noreply.github.com>
2024-04-08 17:10:52 -0500

    17-10

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from animeflv.animeflv import AnimeFLV
 app = Flask(__name__)
 animeflv = AnimeFLV()
 
+@app.route('/')
+def index():
+    return 'Welcome animeflv-api!!'
+
 if __name__ == '__main__':
     app.run(debug=True)
